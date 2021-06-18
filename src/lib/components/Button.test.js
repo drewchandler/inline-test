da595179b8a1e930d85764b851ac5982c1f8d380
Drew Chandler <andrewschandler@gmail.com>
2021-06-18 19:04:31 -0400

    Fix test name

--- src/lib/components/Button.test.js
+++ src/lib/components/Button.test.js
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/svelte";
 import Button from "./Button";
 
-test("shows proper heading when rendered", () => {
+test("shows slot content", () => {
   const { getByText } = render(svelte`<Button>Hello World!</Button>`);
 
   expect(getByText("Hello World!")).toBeInTheDocument();
